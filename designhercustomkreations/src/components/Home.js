83e8db16a313e77735c9f2881d13b602b89fa6e2
Charles <charles.lamb.dev@gmail.com>
2023-12-21 21:51:41 -0500

    added the logo to the home screen

---
+++ designhercustomkreations/src/components/Home.js
@@ -0,0 +1,17 @@
+import './Home.css';
+import React from 'react';
+
+
+function Home() {
+  return (
+    <React.Fragment>
+<header className="mainHeader">
+
+<div className='mainLogo' alt="designHerLogo" src="../public/Designher.jpg" > </div>
+</header>
+    
+        </React.Fragment>
+  );
+}
+
+export default Home;
