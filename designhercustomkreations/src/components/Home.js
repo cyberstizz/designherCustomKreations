2f91432b9dbe8cf4eb728eb3a791c5eb85e5ef65
Charles <charles.lamb.dev@gmail.com>
2023-12-22 22:29:39 -0500

    added all components for the header

--- designhercustomkreations/src/components/Home.js
+++ designhercustomkreations/src/components/Home.js
@@ -8,7 +8,18 @@ function Home() {
 <header className="mainHeader">
 
 <div className='mainLogo' alt="designHerLogo" src="../public/Designher.jpg" > </div>
-<div className='headerRightSide'></div>
+<form className='headerRightSide'>
+    <input type='search' className='searchBar'></input>
+    <ul className='navigationBar'>
+      <li>Jackets</li>
+      <li>Crocs</li>
+    </ul>
+
+    <ul className='navigationBarTwo'>
+      <li>Sneakers</li>
+      <li>Boots</li>
+    </ul>
+</form>
 </header>
     
         </React.Fragment>
