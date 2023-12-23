e57cbc09d3f4afbfd8f9e158653ad5eccd1d23b0
Charles <charles.lamb.dev@gmail.com>
2023-12-23 17:14:25 -0500

    added the video to the homepage

--- designhercustomkreations/src/components/Home.js
+++ designhercustomkreations/src/components/Home.js
@@ -10,17 +10,27 @@ function Home() {
 <div className='mainLogo' alt="designHerLogo" src="../public/Designher.jpg" > </div>
 <form className='headerRightSide'>
     <input type='search' className='searchBar'></input>
-    <ul className='navigationBar'>
-      <li>Jackets</li>
-      <li>Crocs</li>
-    </ul>
+    <div className='navigationBar'>
+      <ul className='navigationBar'>
+        <li>Jackets</li>
+        <li>Crocs</li>
+      </ul>
+    </div>
 
-    <ul className='navigationBarTwo'>
-      <li>Sneakers</li>
-      <li>Boots</li>
-    </ul>
+    <div className='navigationBarTwo'>
+       <ul className='navigationBar'> 
+        <li>Sneakers</li>
+        <li>Boots</li>
+      </ul>
+    </div>
 </form>
 </header>
+
+<video className="homeVideo" width="100%" height="50%" loop="true" autoPlay muted>
+  <source src="/output_video.mp4" type="video/mp4"/>
+  Your browser does not support the video tag.projects/designherCustomKreations/designhercustomkreations/public/VID_158480824_122841_103.mp4
+</video>
+
     
         </React.Fragment>
   );
